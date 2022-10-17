1377d4d894b82cab4b82441d7f327a88519c6240
Snowful <lolitsaburrito@gmail.com>
2022-10-17 12:54:47 -0700

    init impl of assignment 4, cors boilerplate

--- src/main/java/com/cst438/controller/CourseController.java
+++ src/main/java/com/cst438/controller/CourseController.java
@@ -2,16 +2,21 @@ package com.cst438.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.CourseDTOG.GradeDTO;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class CourseController {
 	
 	@Autowired
@@ -23,9 +28,11 @@ public class CourseController {
 	@PutMapping("/course/{course_id}")
 	@Transactional
 	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
-		
-		//TODO  complete this method in homework 4
-		
+		for (GradeDTO gradeDTO : courseDTO.grades) {
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(gradeDTO.student_email, course_id);
+			enrollment.setCourseGrade(gradeDTO.grade);
+			enrollmentRepository.save(enrollment);
+		}
 	}
 
 }
